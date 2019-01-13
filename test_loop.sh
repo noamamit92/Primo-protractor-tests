5c07bad0336601df7087879e72de6ec702662923
gilax <gilad.wolf@mail.huji.ac.il>
2019-01-13 15:56:33 +0200

    debug bash

--- test_loop.sh
+++ test_loop.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cat params.json
 for param in $(jq '.[]' params.json); do
+    echo '$param'
     baseUrl=$(jq '.baseUrl' <<< '$param')
     vid=$(jq '.vid' <<< '$param')
     isVe=$(jq '.isVe' <<< '$param')

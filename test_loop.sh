80cbba60b2adb9ccaba1fd6bb253b83b59b1d5ea
gilax <gilad.wolf@mail.huji.ac.il>
2019-01-13 15:49:24 +0200

    fix bash

--- test_loop.sh
+++ test_loop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-for param in $(jq '.[]' -f params.json); do
-    baseUrl=$(jq -r '.baseUrl' <<< '$param')
-    vid=$(jq -r '.vid' <<< '$param')
-    isVe=$(jq -r '.isVe' <<< '$param')
+for param in $(jq '.[]' params.json); do
+    baseUrl=$(jq '.baseUrl' <<< '$param')
+    vid=$(jq '.vid' <<< '$param')
+    isVe=$(jq '.isVe' <<< '$param')
     protractor tmp/protractor.conf.js --params.baseUrl="$baseUrl" --params.vid="$vid" --params.isVe="$isVe"
 done
